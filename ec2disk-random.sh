0e8007f516539b8ffaabbc642f34ce30ee9d0aae
plamenpasliev <plamen_pasliev@abv.bg>
2018-11-29 18:21:40 +0100

    Update ec2disk-random.sh

--- ec2disk-random.sh
+++ ec2disk-random.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-(date +%s; echo ","; (/usr/bin/time -v /root/fio*/fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=100mb --readwrite=randrw --rwmixread=75)  2>&1 >/dev/null | grep 'Elapsed' | cut -d ' ' -f 8 | cut -d ':' -f 2;) | tr "\n" " ">>  fio.csv
+(date +%s; echo ","; (/usr/bin/time -v /root/fio*/fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=100mb --readwrite=randrw --rwmixread=75)  2>&1 >/dev/null | grep 'Elapsed' | cut -d ' ' -f 8 | cut -d ':' -f 2;) | tr "\n" " ">>  disk-random.csv
 echo>>disk-random.csv
 
 
