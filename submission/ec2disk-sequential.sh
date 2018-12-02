7db76b8843ebbb76f5758314e439ccc547ace162
unknown <csaba.rohrer@mail.polimi.com>
2018-12-02 16:11:10 +0100

    changing ending

---
+++ submission/ec2disk-sequential.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+(date +%s; echo ","; (/usr/bin/time -v  dd if=/dev/zero bs=64000 count=10000 of=file_1GB)  2>&1 >/dev/null | grep 'Elapsed' | cut -d ' ' -f 8 | cut -d ':' -f 2;) | tr "\n" " ">>dd.csv
+echo>>dd.csv
+
+
